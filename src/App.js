d9b7cacdc18d542c484c0a6a3d1037f9f6c04cd4
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-04-01 21:42:53 +0600

    react routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
